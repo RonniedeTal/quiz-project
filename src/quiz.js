d7e6ea005b7ff1ccb7bd914b58a808481fc65f72
Ronnie Vélez <ronnievelz@gmail.com>
2024-04-16 17:49:34 +0200

    casi entero

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,60 @@
 class Quiz {
     // YOUR CODE HERE:
-    //
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
     // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
+    getQuestion() {
+        if (this.currentQuestionIndex >= 0 && this.currentQuestionIndex < this.questions.length) {
+          return this.questions[this.currentQuestionIndex];
+        }
+     }
     
-    // 3. moveToNextQuestion()
+     moveToNextQuestion(){
+      if (this.currentQuestionIndex < this.questions.length - 1) {
+        this.currentQuestionIndex++;
+      } else {
+        console.log("You have reached the end of the quiz"); 
+      }
+     }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+      for (let i = this.questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]]; 
+      }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer){
+     
+     let preguntaActual=this.questions[this.currentQuestionIndex]
+     console.log(preguntaActual)
+     let respuestaJugador=answer
 
-    // 6. hasEnded()
+      if (preguntaActual.answer===respuestaJugador) {
+           this.correctAnswers++;
+           console.log("Correct!");
+         } else {
+           console.log("Incorrect. The correct answer is:", currentQuestion.correctAnswer);
+         }
+       }
+
+     hasEnded(){
+
+     }
 }
+
+
+
+
+
+
+
+const vegetales=["patata", "cebolla", "tomate"]
+let vegIndex=1
+console.log(vegetales[vegIndex])
